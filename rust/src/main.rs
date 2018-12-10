f832b0bae9aba16cddf619359695a3b4523e2fb8
Hamilton <hfantin@gmail.com>
2018-12-10 16:01:34 -0200

    inclui validacao de erros no rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,6 +6,7 @@ use std::time::{Instant};
 use rand::{thread_rng,Rng};
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     // parametros da linha de comando
@@ -21,6 +22,16 @@ fn main() {
     let mut lista = gerar_lista(10, potencia);
     println!("ordena lista com tamanho {} (10^{})", lista.len(), potencia);
 
+    /* outros testes com array */
+    // let mut rng = rand::thread_rng();
+    // let total = (10 as u32).pow(potencia);
+    // let mut array = Vec::new();
+    // for i in 0..total {
+    //     //array[i] = rng.gen::<i32>();
+    //     array.push(i);
+    // }
+    // println!("{:?}", array);
+
     println!("embaralhando...");
     thread_rng().shuffle(&mut lista);
 
@@ -30,24 +41,26 @@ fn main() {
     lista.sort();
 
     let tempo_total = now.elapsed();
+    let sec = (tempo_total.as_secs() as f64) + (tempo_total.subsec_nanos() as f64 / 1000_000_000.0);
 
-    println!("tempo total: {}s e {} ns", tempo_total.as_secs(), decimal_mark(tempo_total.subsec_nanos().to_string()));
+    // println!("tempo total: {}s e {} ns", tempo_total.as_secs(), decimal_mark(tempo_total.subsec_nanos().to_string()));
+    println!("tempo total: {}s", sec);
 
     grava_arquivo(&mut lista, "output.txt", gravar);
 }
 
-fn decimal_mark(s: String) -> String {
-    let mut result = String::with_capacity(s.len() + ((s.len() - 1) / 3));
-    let mut i = s.len();
-    for c in s.chars() {
-        result.push(c);
-        i -= 1;
-        if i > 0 && i % 3 == 0 {
-            result.push('.');
-        }
-    }
-    result
-}
+// fn decimal_mark(s: String) -> String {
+//     let mut result = String::with_capacity(s.len() + ((s.len() - 1) / 3));
+//     let mut i = s.len();
+//     for c in s.chars() {
+//         result.push(c);
+//         i -= 1;
+//         if i > 0 && i % 3 == 0 {
+//             result.push('.');
+//         }
+//     }
+//     result
+// }
 
 fn gerar_lista(base: u32, potencia: u32) -> Vec<u32> {
     let total = base.pow(potencia);
@@ -57,12 +70,18 @@ fn gerar_lista(base: u32, potencia: u32) -> Vec<u32> {
 fn le_console() -> u32 {
     println!("Digite a quantidade de elementos(10 ^ x): ");
     let mut input = String::new();
-    io::stdin()
-        .read_line(& mut input)
-        .expect("Falha na leitura da linha");
-    return input
-        .trim()
-        .parse::<u32> ().unwrap();
+    io::stdin().read_line(& mut input).expect("Falha na leitura da linha");
+    let is_number = &input.trim().parse::<u32>();
+    match is_number {
+        Ok(value) => *value, 
+        Err(e) => {
+            // eprintln!("erro: {:?}", e);
+            // 1
+            // panic!("{}", e)
+            println!("{:?}", e);
+            process::exit(1)
+        },
+    }
 }
 
 fn grava_arquivo(lista: &mut Vec<u32>, arquivo: &str, gravar: bool) {
